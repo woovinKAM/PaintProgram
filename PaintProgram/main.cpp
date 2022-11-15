a6192f8550bbef55820e57c67e1632c133fe8ced
woovinKAM <woombii@gmail.com>
2022-11-15 22:02:08 +0900

    Initial Commit

---
+++ PaintProgram/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  PaintProgram
+//
+//  Created by 감우빈 on 2022/11/15.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
